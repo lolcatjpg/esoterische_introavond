ddcc03e966bae959f15f203670e4dcd7f4d4a13d
Iwijn <iwijn.voeten@gmail.com>
2023-10-11 21:12:54 +0200

    simple assembly

---
+++ 2023/2/iwijn.asm
@@ -0,0 +1,14 @@
+section	.text
+	global _start       ;must be declared for using gcc
+_start:                     ;tell linker entry point
+	mov	edx, len    ;message length
+	mov	ecx, msg    ;message to write
+	mov	ebx, 1	    ;file descriptor (stdout)
+	mov	eax, 4	    ;system call number (sys_write)
+	int	0x80        ;call kernel
+	jmp _start
+
+section	.data
+
+msg	db	'Zeus',0xa	;our dear string
+len	equ	$ - msg			;length of our dear string
